b8196d55b0018b804f80ae5c8874c054c0948dea
EmreOz <emreoz3734@gmail.com>
2020-02-11 12:07:14 +0300

    Nuget Package Reference Version Downgrade and Added Data Structure

---
+++ Data/Mapping/Blogs/BlogCommentMap.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Core.Domain.Blogs;
+
+namespace Data.Mapping.Blogs
+{
+    /// <summary>
+    /// Represents a blog comment mapping configuration
+    /// </summary>
+    public partial class BlogCommentMap : EntityTypeConfiguration<BlogComment>
+    {
+        #region Methods
+
+        /// <summary>
+        /// Configures the entity
+        /// </summary>
+        /// <param name="builder">The builder to be used to configure the entity</param>
+        public override void Configure(EntityTypeBuilder<BlogComment> builder)
+        {
+            builder.ToTable(nameof(BlogComment));
+            builder.HasKey(comment => comment.Id);
+
+            builder.HasOne(comment => comment.BlogPost)
+                .WithMany(blog => blog.BlogComments)
+                .HasForeignKey(comment => comment.BlogPostId)
+                .IsRequired();
+
+            base.Configure(builder);
+        }
+
+        #endregion
+    }
+}
